cdc7c8114326d127bda5ce751fd1dd534477b020
Umoh Samuel <samuelwisdom360@gmail.com>
2024-08-11 21:24:28 +0100

    Add project files.

---
+++ Ch2/Main.cpp
@@ -0,0 +1,8 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+	cout << "Hello, programming!\n" << "Here we go!\n";
+	keep_window_open();
+	return 0;
+}
---
+++ Ch3/Main.cpp
@@ -0,0 +1,63 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+	// input, variables and types
+	/*cout << "Enter your first name and age: ";
+	string first_name = "???";
+	int age = 0;
+	cin >> first_name;
+	cin >> age;
+	double age_in_months = age * 12;
+	cout << "Name " << first_name << " nice to meet you \n";
+	cout << "You are " << age_in_months << " months old \n";*/
+	
+	// operations and operands
+	/*cout << "Please enter a positive Integer value(n): ";
+	int n = 0;
+	int m = 2;
+	cin >> n;
+	cout << "n == " << n
+		<< "\nn+1 == " << n + 1
+		<< "\nthree times n == " << 3 * n
+		<< "\ntwice n == " << n + n
+		<< "\nn squared == " << n * n
+		<< "\nhalf of n == " << n / 2
+		<< "\nsquare root of n == " << sqrt(n)
+		<< "\nm ==" << m
+		<< "\nn/m * m + n % m == " << n / m * m + n % m
+		<< "\n";*/
+	/*cout << "Enter your first and last name\n";
+	string first;
+	string last;
+	cin >> first >> last;
+	if (first == last) cout << "that's the same name twice\n";
+	if (first < last)
+		cout << first << " is alphabetically before " << last << "\n";
+	if (first > last)
+		cout << first << " is alphabetically after " << last << "\n";
+	string name = first + ' ' + last;
+	cout << "Hello, " << name << "\n";*/
+
+	// assignments and initialization
+	// initialization: give a variable it's initial value
+	//int x = 5;
+	//assignment: give a variable a new value
+	//x = 9;
+	//detect repeated words
+	//int number_of_words = 0;
+	//int repeated_words = 0;
+	//string previous = " ";
+	//string current;
+	//while (cin >> current) {
+	//	++number_of_words;
+	//	if (current == previous) {
+	//		++repeated_words;
+	//		cout << "Repeated word: " << current << "\n";
+	//	}
+	//	previous = current;
+	//} // terminate program by giving an end of input character "ctrl + z" on windows, "ctrl + d" on a Linux or Unix machine
+	//cout << "Total number of words: " << number_of_words << "\n" << "Total number of repeated words : " << repeated_words << "\n";
+
+	// names
+}
